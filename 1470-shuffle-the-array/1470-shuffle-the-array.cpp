d86eeef7abea5b039f61cddb825e9d593b5655e0
gourav <gouravswami9785@gmail.com>
2026-07-31 11:39:08 +0530

    Time: 0 ms (100%), Space: 13.8 MB (19.82%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -6,15 +6,34 @@ public:
 
         
          vector<int> ans;
+
+         vector<int> v1;
+         vector<int> v2;
+
+
         
 
 
 
         for(int i=0;i<n;i++){
 
-            ans.push_back(nums[i]);
-            ans.push_back(nums[n+i]);
+            v1.push_back(nums[i]);
 
+
+            // ans.push_back(nums[i]);
+            // ans.push_back(nums[n+i]);
+
+        }
+
+
+        for(int i=n;i<size;i++){
+            v2.push_back(nums[i]);
+        }
+
+
+        for(int i = 0; i < n; i++) {
+            ans.push_back(v1[i]);
+            ans.push_back(v2[i]);
         }
 
 
