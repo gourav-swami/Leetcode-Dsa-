a41c274b058f5c5a783a753808ea9b493032ad16
gourav <gouravswami9785@gmail.com>
2026-08-15 22:21:11 +0530

    Time: 0 ms (100%), Space: 12.2 MB (8.04%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+
+        int n = strs.size();
+        if( n == 0) return strs[0];
+
+        sort ( strs.begin(),strs.end() );
+        string first = strs[0];
+        string last = strs[n-1];
+        string s="";
+
+
+
+        for( int i = 0 ; i < ( min( first.length() , last.length() )); i++){
+
+            if ( first[i]==last[i]) {
+
+                s = s + first[i];
+
+
+            }
+
+            else {
+                break;
+            }
+        }
+
+        return s;
+    }
+};
