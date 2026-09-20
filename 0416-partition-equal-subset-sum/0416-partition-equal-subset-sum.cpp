34dc368e10782f9d4d6536c4a3d50165f41ef7e3
gourav <gouravswami9785@gmail.com>
2026-09-20 17:09:48 +0530

    Time: 102 ms (70.85%), Space: 39.1 MB (50.26%) - LeetHub

---
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+
+
+bool isSubsetSum(vector<int>& arr, int sum) {
+        // code here
+        
+        int n = arr.size();
+        
+        bool** t = new bool*[n+1];
+
+            // 2. Allocate an array for each individual row
+            for (int i = 0; i < n+1; ++i) {
+                t[i] = new bool[sum+1];
+            }
+            
+            for( int i =0; i<n+1;i++){
+                
+                for(int j=0; j<sum+1;j++){
+                    
+                    
+                    if(i==0){
+                        t[i][j] = false;
+                    }
+                    
+                    if(j==0){
+                        t[i][j] = true;
+                        
+                    }
+                }
+            }
+         
+            for(int i=1;i<n+1;i++){
+                
+                
+                for(int j=1;j<sum+1;j++){
+                    
+                if(arr[i-1]<=j){
+                    
+                t[i][j] = t[i-1][j-arr[i-1]] || t[i-1][j];
+                }
+            
+            else{
+                t[i][j] = t[i-1][j];
+                }
+                }
+            }           
+            return t[n][sum];    
+    }
+    bool canPartition(vector<int>& arr) {
+
+        int n=arr.size();
+
+        int sum =0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+
+        }
+
+        if(sum%2!=0){
+            return false;
+        }
+
+        else{
+            return isSubsetSum(arr,sum/2);
+        }
+
+
+        
+    }
+};
