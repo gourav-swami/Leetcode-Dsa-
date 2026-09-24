6fce4957d7a6f5afb2b40ffd94eb1f887c7e0c94
gourav <gouravswami9785@gmail.com>
2026-09-25 00:53:58 +0530

    Time: 23 ms (83.74%), Space: 26.9 MB (81.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,56 +1,43 @@
-int static t[1001][1001];
+
 
 class Solution {
 public:
-
-
-
-   
-
-int longestCommonSubsequence1(string &x, string &y,int n,int m) {
-
-        
-
-        // Base condition
-
-        if(n==0 || m==0){
-            return 0;
-        }
-
-        if(t[n][m] !=-1){
-            return t[n][m];
-        }
-
-
-
-
-
-        if(x[n-1] == y[m-1]){
-            
-            t[n][m] = 1+longestCommonSubsequence1(x,y,n-1,m-1);
-        }
-
-        else{
-            t[n][m] = max(longestCommonSubsequence1(x,y,n,m-1) , longestCommonSubsequence1(x,y,n-1,m));
-        }
-
-
-        return t[n][m];
-
-
-        
-    }
     int longestCommonSubsequence(string x, string y) {
 
-        int n = x.length();
-        int m = y.length();
+        int m = x.length();
+        int n = y.length();
 
-         memset(t,-1,sizeof(t));
+        int** t = new int*[m+1];
 
-        int ans = longestCommonSubsequence1(x,y,n,m);
-        return ans;
+    // 2. Allocate memory for columns in each row
+    for (int i = 0; i <= m; ++i) {
+        t[i] = new int[n+1];
+    }
 
-        
+
+    for(int i=0;i<=m;i++){
+        for(int j=0;j<=n;j++){
+
+            if(i==0||j==0){
+                t[i][j] = 0;
+            }
+        }
+    }
+
+    for(int i=1;i<m+1;i++){
+        for(int j=1;j<n+1;j++){
+            if(x[i-1] == y[j-1]){
+                t[i][j] = 1+t[i-1][j-1];
+            }
+
+            else{
+                t[i][j] = max(t[i-1][j] , t[i][j-1]);
+            }
+        }
+    }
+
+    return t[m][n];
+
 
         
     }
