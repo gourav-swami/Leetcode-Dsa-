6204ce6b0515bdb5acd5a482691b9ad797f69965
gourav <gouravswami9785@gmail.com>
2026-09-25 15:42:55 +0530

    Time: 0 ms (100%), Space: 50.3 MB (33.16%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -4,24 +4,22 @@ public:
 
         int n=arr.size();
 
-        int ans=0;
-        int count=0;
+        int maxcount =0;
+        int count =0;
 
-        for( int i=0;i<n;i++){
+        for(int i=0;i<n;i++){
             if(arr[i]==1){
                 count++;
-                ans=max(ans,count);
+                maxcount = max(maxcount ,count);
             }
 
             else{
-                count=0;
+                count =0;
             }
-
-            
-
         }
 
-        return ans;
+        return maxcount ;
+        
     
     }
 };
